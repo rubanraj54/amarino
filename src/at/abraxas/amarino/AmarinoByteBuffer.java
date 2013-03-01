7e92d821c788702aa9c7f3b7cee6f364416b7cd6
matthias.vavti <matthias.vavti@gmail.com>
2013-03-01 10:34:23 +0000

    Completed the ByteBuffer Framework (not fully tested yet)

--- src/at/abraxas/amarino/AmarinoByteBuffer.java
+++ src/at/abraxas/amarino/AmarinoByteBuffer.java
@@ -4,6 +4,7 @@ public class AmarinoByteBuffer {
 	
 	private byte[] buffer;
 	private int pointer = 0;
+	public final String EOS_MARKER = ";";
 	
 	public AmarinoByteBuffer(int capacity){
 		buffer = new byte[capacity];
@@ -15,13 +16,13 @@ public class AmarinoByteBuffer {
 		AmarinoByteBuffer bb = new AmarinoByteBuffer(16);
 
 		
-		bb.put((byte) 2);
+		bb.put((byte) 1);
 		bb.put((byte) 0);
+		bb.put((byte) 1);
 		bb.put((byte) 0);
-		bb.put((byte) 0);
-		bb.put((byte) 5);
+		bb.put((byte) 1);
 		
-		System.out.println(bb.getIntArray(2)[1]);
+		System.out.println(bb.getBooleanArray(5)[4]);
 		
 	}
 	
@@ -44,6 +45,11 @@ public class AmarinoByteBuffer {
 		pointer = 0;
 	}
 	
+	/**
+	 * Int in Arduino is 4 bytes
+	 * @param pos
+	 * @return
+	 */
 	public int getInt(int pos) {
 		String s = "";
 		for(int i = 0; i < 4; i++)
@@ -52,6 +58,11 @@ public class AmarinoByteBuffer {
 	    return new Integer(s);
 	}
 	
+	/**
+	 * Int in Arduino is 4 bytes
+	 * @param pos
+	 * @return
+	 */
 	public int[] getIntArray(int numValues){
 		int[] ints = new int[numValues];
 		
@@ -67,19 +78,30 @@ public class AmarinoByteBuffer {
 		return ints;
 	}
 	
-	public long getLong(int pos){
-		
-	}
+//	public long getLong(int pos){
+//		return -1;
+//		
+//	}
+//	
+//	public long[] getLongArray(int numValues){
+//		return new long[numValues];
+//	}
 	
-	public long[] getLongArray(int numValues){
-		
-	}
-	
+	/**
+	 * Boolean in Arduino is 1 byte (0 or 1)
+	 * @param pos
+	 * @return
+	 */
 	public boolean getBoolean(int pos){
 		if(buffer[pos] == 1) return true;
 		else return false;
 	}
 	
+	/**
+	 * Boolean in Arduino is 1 byte (0 or 1)
+	 * @param pos
+	 * @return
+	 */
 	public boolean[] getBooleanArray(int numValues){
 		boolean[] bools = new boolean[numValues];
 		
@@ -109,38 +131,122 @@ public class AmarinoByteBuffer {
 		return bytes;
 	}
 	
+	/**
+	 * Chars in Arduino have 1 byte
+	 * @param pos
+	 * @return
+	 */
 	public char getChar(int pos){
-		
+		return (char) buffer[pos];
 	}
 	
+	/**
+	 * Chars in Arduino have 1 byte
+	 * @param numValues
+	 * @return
+	 */
 	public char[] getCharArray(int numValues){
+		char[] chars = new char[numValues];
 		
+		for(int i = 0; i < numValues; i++){
+			if(buffer[i] != -1)
+				chars[i] = (char) buffer[i];
+		}
+		
+		return chars;
 	}
 	
+	/**
+	 * Float in Arduino is float in Android (4 bytes)
+	 * @param pos
+	 * @return
+	 */
 	public float getFloat(int pos){
-		
+		String s = "";
+		for(int i = 0; i < 4; i++)
+			s+=buffer[4*pos+i];
+	    
+	    return new Float(s);
 	}
 
+	/**
+	 * Float in Arduino is float in Android (4 bytes)
+	 * @param pos
+	 * @return
+	 */
 	public float[] getFloatArray(int numValues){
+		float[] floats = new float[numValues];
 		
+		for(int i = 0; i < numValues; i++){
+			String s = "";
+			for(int j = 0; j < 4; j++){
+				if(buffer[i+j] != -1)
+					s+=buffer[i+j];
+			}
+			
+			floats[i] = new Float(s);
+		}		
+		return floats;
 	}
 	
+	/**
+	 * Short in Arduino are 2 bytes
+	 * @param pos
+	 * @return
+	 */
 	public short getShort(int pos){
-		
+		String s = "";
+		for(int i = 0; i < 2; i++)
+			s+=buffer[2*pos+i];
+	    
+	    return new Short(s);
 	}
 	
+	/**
+	 * Short in Arduino are 2 bytes
+	 * @param pos
+	 * @return
+	 */
 	public short[] getShortArray(int numValues){
+		short[] shorts = new short[numValues];
 		
+		for(int i = 0; i < numValues; i++){
+			String s = "";
+			for(int j = 0; j < 2; j++){
+				if(buffer[i+j] != -1)
+					s+=buffer[i+j];
+			}
+			
+			shorts[i] = new Short(s);
+		}		
+		return shorts;
 	}
 	
 	public String getString(int pos){
 		//TODO: for Strings we need to transmit an EOS (End of String) indicator to know when the String ends
 		//		for Example ";"
+		
+		String s = "";
+		for(int i = 0; i < buffer.length; i++)
+			s+= buffer[i]; //assuming chars from Arduino have only 1 byte
+		
+		String[] strings = s.split(EOS_MARKER);
+
+		return strings[pos];
 	}
 	
 	public String[] getStringArray(int numValues){
 		//TODO: for String Arrays we need a Delimiter between Strings (take EOS indicator)
 		
+		String s = "";
+		for(int i = 0; i < buffer.length; i++)
+			s+= buffer[i]; //assuming chars from Arduino have only 1 byte
+		
+		String[] strings = s.split(EOS_MARKER);
+		
+		return strings;
+		
+
 	}
 }
 
